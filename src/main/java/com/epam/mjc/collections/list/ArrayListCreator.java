bb9268cdc04743949280d5d46574d99ecfceff21
Denis <denis.li@payme.uz>
2024-03-14 21:57:37 +0500

    feat: implemented first task

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,12 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+	    ArrayList<String> resultList = new ArrayList<String>();
+	    for (int i = 0; i < sourceList.size(); i++) {
+		    if(i+1 % 3 == 0) {
+			    resultList.add(sourceList[i]);
+		    }
+	    }
+	    return resultList;
     }
 }
